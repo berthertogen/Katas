d2e4d9257b5673acdaf0067b6f53a3bbbfb8a8b8
Bert Hertogen <bert.hertogen@live.be>
2018-04-08 20:27:52 +0200

    PrimeFactors CSharp Initial commit

---
+++ PrimeFactors.CSharp/PrimeFactor.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PrimeFactors.CSharp
+{
+    public class PrimeFactor
+    {
+        public static List<int> Generate(int number)
+        {
+            return new List<int>();
+        }
+
+    }
+}
---
+++ PrimeFactors.CSharp/PrimeFactorTests.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using Xunit;
+
+namespace PrimeFactors.CSharp
+{
+    public class PrimeFactorTests
+    {
+        [Fact]
+        public void TestOne()
+        {
+            Assert.Equal(new List<int>(), PrimeFactor.Generate(1));
+        }
+    }
+}
